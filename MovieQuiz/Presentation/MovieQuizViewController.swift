f5012d3f52c9fc3b353793db94afa3c8a3400a36
Luba <lubbroccoli@gmail.com>
2024-05-18 22:23:47 +0200

    fixed func show

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,12 +2,6 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     
-    struct ViewModel {
-        let image: UIImage
-        let text: String
-        let questionNumber: String
-    }
-    
     struct QuizStepViewModel {
         let image: UIImage
         let question: String
@@ -31,7 +25,7 @@ final class MovieQuizViewController: UIViewController {
     private let questions: [QuizQuestion] = [
         
         QuizQuestion(
-            image: "The The Godfather",
+            image: "The Godfather",
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true),
         QuizQuestion(image: "The Dark Knight",
@@ -106,20 +100,14 @@ final class MovieQuizViewController: UIViewController {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
-        
-        let currentQuestion = questions[currentQuestionIndex]
-        let preparedImage = convert(model: currentQuestion)
-        show(quiz: preparedImage)
     }
-    
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
         return questionStep
-    }
-    
+    } 
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let text = "Ваш результат: \(correctAnswers)/10"
@@ -136,35 +124,29 @@ final class MovieQuizViewController: UIViewController {
             show(quiz: viewModel)
         }
     }
-    
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
             correctAnswers += 1
         }
+        let answerText = isCorrect ? "ДА" : "НЕТ"
         
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor: UIColor.ypRed.cgColor
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
         }
-        
+    }
+    private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
-            title: "Этот раунд окончен!",
-            message: "Ваш результат ???",
+            title: result.title,
+            message: result.text,
             preferredStyle: .alert)
-        
-        /* private func show(quiz result: QuizResultsViewModel) {
-         let alert = UIAlertController(
-         title: result.title,
-         message: result.text,
-         preferredStyle: .alert)*/
         
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
-            
             let firstQuestion = self.questions[self.currentQuestionIndex]
             let viewModel = self.convert(model: firstQuestion)
             self.show(quiz: viewModel)
@@ -173,6 +155,21 @@ final class MovieQuizViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
+            
+            
+        /*let currentQuestion = questions[currentQuestionIndex]
+        let preparedImage = convert(model: currentQuestion)
+        show(quiz: preparedImage)*/
+    
+    
+    
+        /*return questionStep*/
+    /*let alert = UIAlertController(
+                title: "Этот раунд окончен!",
+                message: "Ваш результат: \(answerText)",
+                preferredStyle: .alert)
+            */
+        
     //MARK: - Отображение
     
     @IBOutlet private var imageView: UIImageView!
